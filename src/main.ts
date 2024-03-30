5e9182d8598be9d4539da05bd3df62c2145d9889
rigelHadushiDev <rigelhadushi4@gmail.com>
2024-03-30 14:43:12 +0100

    Personal project creation

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'] 
+  };
+
+
+  app.enableCors(corsOptions);
+
+  
+}
+bootstrap();
+
