b11ef4cb61f5fac9a433a70792e7f9e1572e4a2b
rigelHadushiDev <rigelhadushi4@gmail.com>
2024-04-08 18:43:30 +0200

    Jwt secret is protected -bug fixed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from 'src/user/user.module';
-
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
     UsersModule,
     JwtModule.register({
-      secret: 'secret',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '100h' },
     }),
   ],
