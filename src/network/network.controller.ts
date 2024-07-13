2f3c81fd714cc9273ae99181bf0f9d30fc43b433
rigelHadushiDev <rigelhadushi4@gmail.com>
2024-07-14 01:21:44 +0200

    network Module dev

--- src/network/network.controller.ts
+++ src/network/network.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, NotFoundException, Param, Post } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { NetworkService } from './network.service';
+import { ApiException } from '@nanogiants/nestjs-swagger-api-exception-decorator';
+import { GeneralResponse } from 'src/post/dtos/GeneralResponse';
 
+
+@ApiBearerAuth()
+@ApiTags('Network Module')
 @Controller('network')
-export class NetworkController {}
+export class NetworkController {
+
+    constructor(private readonly networkService: NetworkService) { }
+
+
+    @Post('follow/:followeeId')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Follow a user', description: 'Create a new connection.' })
+    @ApiException(() => NotFoundException, { description: 'User with this Id not found.  [key: "userNotFound" ]' })
+    @ApiParam({ name: 'followeeId', description: 'ID of the user that you want to follow' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'The user has been successfully followed.', type: GeneralResponse })
+    async followUser(@Param() followeeId: number) {
+        return await this.networkService.followUser(followeeId);
+    }
+
+    @Post('unfollow/:followeeId')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Unfollow a user', description: 'Unfollow a new connection.' })
+    @ApiException(() => NotFoundException, { description: 'User with this Id not found.  [key: "userNotFound" ]' })
+    @ApiException(() => NotFoundException, { description: 'Cannot unfollow a user which is not part of your network.  [key: "networkConnectionNotFound" ]' })
+    @ApiParam({ name: 'followeeId', description: 'ID of the user that you want to unfollow' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'The user has been successfully unfollowed.', type: GeneralResponse })
+    async unfollowUser(@Param() followeeId: number) {
+        return await this.networkService.followUser(followeeId);
+    }
+
+
+    @Post('remove/:followeeId')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: 'Follow a user', description: 'Remove a connection.' })
+    @ApiException(() => NotFoundException, { description: 'User with this Id not found.  [key: "userNotFound" ]' })
+    @ApiException(() => NotFoundException, { description: 'Cannot remove a user which is not part of your network.  [key: "cantRemoveNonFollowingUser" ]' })
+    @ApiParam({ name: 'followeeId', description: 'ID of the friend that you want to remove' })
+    @ApiResponse({ status: HttpStatus.OK, description: 'Successfully removed a friend connection from your account. ', type: GeneralResponse })
+    async removeConnection(@Param() followeeId: number) {
+        return await this.networkService.removeConnection(followeeId);
+    }
+
+
+
+
+}
--- src/network/network.service.ts
+++ src/network/network.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { UserProvider } from 'src/user/user.provider';
 import { Network } from './entities/network.entity';
 import { EntityManager } from 'typeorm';
@@ -22,50 +22,47 @@ export class NetworkService {
         let user = await this.entityManager
             .createQueryBuilder()
             .from(User, 'user')
-            .select('isPrivate')
+            .select('*')
             .where("user_id = :followeeId", { followeeId: followeeId })
+            .andWhere('archieve = :archieveStatus', { archieveStatus: false })
             .getOne();
 
 
-        if (user) {
-
-            if (user?.isPrivate) {
-
-                await this.entityManager
-                    .createQueryBuilder()
-                    .insert()
-                    .into(Network)
-                    .values({
-                        followerId: userId,
-                        followeeId,
-                        createdAt: new Date(),
-                    })
-                    .returning('*')
-                    .execute();
-
-                resp.message = "userReqToBeMyNetwork";
-
-            } else {
-                await this.entityManager
-                    .createQueryBuilder()
-                    .insert()
-                    .into(Network)
-                    .values({
-                        followerId: userId,
-                        followeeId,
-                        pending: true
-                    })
-                    .returning('*')
-                    .execute();
-
-                resp.message = "userAddedInNetwork";
-            }
-            resp.status = HttpStatus.OK;
-        }
-        else {
+        if (!user)
             throw new NotFoundException(`userNotFound`);
+
+        if (user?.isPrivate) {
+
+            await this.entityManager
+                .createQueryBuilder()
+                .insert()
+                .into(Network)
+                .values({
+                    followerId: userId,
+                    followeeId,
+                    pending: true
+                })
+                .execute();
+
+            resp.message = "userReqToBeFollowed";
+
+        } else {
+
+            await this.entityManager
+                .createQueryBuilder()
+                .insert()
+                .into(Network)
+                .values({
+                    followerId: userId,
+                    followeeId,
+                    createdAt: new Date()
+                })
+                .execute();
+
+            resp.message = "userSuccessfullyFollowed";
         }
 
+        resp.status = HttpStatus.OK;
         return resp;
     }
 
@@ -78,55 +75,148 @@ export class NetworkService {
         let user = await this.entityManager
             .createQueryBuilder()
             .from(User, 'user')
-            .select('isPrivate')
+            .select('*')
             .where("user_id = :followeeId", { followeeId: followeeId })
+            .andWhere('archieve = :archieveStatus', { archieveStatus: false })
             .getOne();
 
+        if (!user)
+            throw new NotFoundException(`userIsNotFound`);
 
-        if (user) {
+        const networkConnection = await this.entityManager
+            .createQueryBuilder()
+            .from(Network, 'network')
+            .select(['networkId', 'pending'])
+            .where("followerId = :userId", { userId: currUserId })
+            .andWhere('deleted = :deletedStatus', { deletedStatus: false })
+            .getOne();
 
-            const networkConnection = await this.entityManager
-                .createQueryBuilder()
-                .from(Network, 'network')
-                .select(['networkId', 'pending'])
-                .where("followerId = :userId", { userId: currUserId })
-                .getOne();
+        if (!networkConnection)
+            throw new NotFoundException(`networkConnectionNotFound`);
 
-            if (networkConnection) {
 
-                await this.entityManager
-                    .createQueryBuilder()
-                    .update(Network)
-                    .set({ deleted: true })
-                    .where("followerId = :userId", { userId: currUserId })
-                    .andWhere('followeeId = :followeeId', { followeeId })
-                    .execute();
+        await this.entityManager
+            .createQueryBuilder()
+            .update(Network)
+            .set({ deleted: true })
+            .where("followerId = :userId", { userId: currUserId })
+            .andWhere('followeeId = :followeeId', { followeeId })
+            .andWhere('deleted = :deletedStatus', { deletedStatus: false })
+            .execute();
 
-                resp.status = HttpStatus.OK;
-                resp.message = networkConnection?.pending ? "userRevokedTheRequest" : "userRemovedFromNetwork";
+        resp.status = HttpStatus.OK;
+        resp.message = networkConnection?.pending ? "userRevokedTheRequest" : "userUnfollowedSuccessfully";
 
-            } else {
-                throw new NotFoundException(`networkConnectionNotFound`);
-            }
-        }
-        else {
-            throw new NotFoundException(`userNotFound`);
-        }
         return resp;
     }
 
 
-    async removeUser(followeeId: number) {
+    async removeConnection(followeeId: number) {
+
+        const currUserID = this.UserID;
+        let resp = new GeneralResponse();
 
         let user = await this.entityManager
             .createQueryBuilder()
             .from(User, 'user')
             .select('isPrivate')
             .where("user_id = :followeeId", { followeeId: followeeId })
+            .andWhere('archieve = :archieveStatus', { archieveStatus: false })
             .getOne();
-
 
         if (!user)
             throw new NotFoundException(`userNotFound`);
+
+        let networkConnection = await this.entityManager
+            .createQueryBuilder()
+            .from(Network, 'network')
+            .select('*')
+            .where("followerId = :userId AND followeeId = :followeeId AND deleted = :deletedStatus", {
+                userId: currUserID,
+                followeeId: followeeId,
+                deletedStatus: false
+            })
+            .orWhere("followerId = :followeeId AND followeeId = :userId AND deleted = :deletedStatus", {
+                followeeId: followeeId,
+                userId: currUserID,
+                deletedStatus: false
+            })
+            .getMany();
+
+        if (networkConnection?.length === 0)
+            throw new BadRequestException(`cantRemoveNonFollowingUser`)
+
+        for (const connection of networkConnection) {
+
+            await this.entityManager
+                .createQueryBuilder()
+                .update(Network)
+                .set({ deleted: true })
+                .where("networkId = :networkId", { networkId: connection?.networkId })
+                .andWhere('deleted = :deletedStatus', { deletedStatus: false })
+                .execute();
+
+        }
+
+        resp = { status: HttpStatus.OK, message: "userRemovedSuccessfully" };
+        return resp;
+
     }
+
+    // kethji ne nje metode qe merr count quaje ConnectionsCount dhe do kesh followersCount dhe followintgCount
+    async followingCount(followeeId: number) {
+
+        let currUserID: number = this.UserID;
+
+        // zevenedesoj me response qe do te ket followingCounter
+        let resp = { followingCounter: 0 }
+
+        let followingCount = await this.entityManager
+            .createQueryBuilder()
+            .from(Network, 'network')
+            .select('COUNT(*)', 'counter')
+            .where("followerId = :userId AND followeeId = :followeeId AND deleted = :deletedStatus", {
+                userId: currUserID,
+                followeeId: followeeId,
+                deletedStatus: false
+            })
+            .getRawOne();
+
+        let counter = followingCount?.counter || 0;
+        resp = counter;
+
+        return counter;
+    }
+
+
+
+    async followersCount(followeeId: number) {
+
+        let currUserID: number = this.UserID;
+
+        // zevenedesoj me response qe do te ket followersCount
+        let resp = { followersCount: 0 }
+
+        let followersCount = await this.entityManager
+            .createQueryBuilder()
+            .from(Network, 'network')
+            .select('COUNT(*)', 'counter')
+            .where("followerId = :followeeId AND followeeId = :userId AND deleted = :deletedStatus", {
+                followeeId: followeeId,
+                userId: currUserID,
+                deletedStatus: false
+            })
+            .getRawOne();
+
+        let counter = followersCount?.counter || 0;
+        resp = counter;
+
+        return counter;
+    }
+
+    // metodat getFolloers/getFollowing me pagination getFollowing 
+    //shiko nese jan public/vs privat nese jane privat athere nqs i ke connection mund ti shikosh
+    // shiko nese useri i ka follow dhe nqs i ka follow do te jete followed true nese jo followed false 
+
+
 }
