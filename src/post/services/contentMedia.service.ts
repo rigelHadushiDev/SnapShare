0be35100f1d5b5c919da41c22636dc2dd0f6dec3
rigelHadushiDev <rigelhadushi4@gmail.com>
2024-07-30 23:27:08 +0200

    getMethods feed get only add 2 comments limit

--- src/post/services/contentMedia.service.ts
+++ src/post/services/contentMedia.service.ts
@@ -28,4 +28,38 @@ export class ContentMediaService {
     // post feed
 
 
+    // WITH PostLikers AS (
+    //     SELECT
+    //         pl."postId",
+    //         STRING_AGG(DISTINCT l."username", ', ') AS "LikersUsernames"
+    //     FROM "postLike" pl
+    //     INNER JOIN "user" l ON l."userId" = pl."userId"
+    //     WHERE pl.deleted = FALSE
+    //     GROUP BY pl."postId"
+    //         LIMIT 3
+    // )
+    // SELECT 
+    // po."postId",
+    //   po."postDescription",
+    //   po."commentsNr",
+    //   po."likesNr",
+    //   po.media,
+    //   po."userId",
+    //   po."createdAt",
+    //   u."profileImg",
+    //   u.username,
+    //   CONCAT(u."firstName", ' ', u."lastName") as "AccFullName",
+    //   CASE 
+    //     WHEN pl."likeId" IS NOT NULL THEN 'true'
+    //     ELSE 'false'
+    //   END AS "LikedByUser",
+    //   COALESCE(pls."LikersUsernames", '') AS "LikersUsernames"
+    // FROM "post" po
+    // INNER JOIN "user" u ON u."userId" = po."userId" AND u.archive = false
+    // LEFT JOIN "network" n ON n."followeeId" = po."userId" AND  n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = 2
+    // LEFT JOIN "postLike" pl ON pl."userId" = n."followerId" AND pl."postId" = po."postId" AND pl.deleted = false
+    // LEFT JOIN PostLikers pls ON pls."postId" = po."postId"
+    // WHERE po.archive = FALSE AND n."networkId" IS NOT NULL;
+
+
 }
