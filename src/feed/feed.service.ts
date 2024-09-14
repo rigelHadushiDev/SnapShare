fbdbe1c3f9ae6d61465b037235283e684addd57b
rigelHadushiDev <rigelhadushi4@gmail.com>
2024-09-15 00:50:42 +0200

    feedBranch dev

--- src/feed/feed.service.ts
+++ src/feed/feed.service.ts
@@ -10,6 +10,7 @@ import { CommentService } from 'src/comment/comment.service';
 import { GetFeedResp } from 'src/post/dtos/getFeed.dto';
 import { StoryViews } from 'src/story/StoryViews.entity';
 import { UserFeed } from './entities/userFeed.entity';
+import { off } from 'process';
 const fs = require('fs');
 
 @Injectable()
@@ -53,265 +54,176 @@ export class FeedService {
     }
 
 
-    async getPostsFeed(postsByPage: number = 10, page: number = 1, postCommentsLimit: number = 3, timeDirection = true) {
+    async getPostsFeed(postsByPage: number = 10, page: number = 1, postCommentsLimit: number = 3) {
 
 
         let resp = new GetFeedResp();
 
         let offset = (page - 1) * postsByPage;
-        let feedDataTOAlgorithm = await this.getDataToFeedAlgorith(timeDirection, postsByPage, offset);
-        // if (timeDirection
+        let posts = await this.getPostsByPriority(postsByPage, offset);
 
-        // posts = await this.getPostsByPriority();
+        let feedContainer = [];
+        for (let post of posts) {
+            let postContainer = [];
 
-        // let feedContainer = [];
-        // for (let post of posts) {
-        //     let postContainer = [];
+            if (post?.postMedia)
+                post.postMedia = SnapShareUtility.urlConverter(post.postMedia);
 
-        //     if (post?.postMedia)
-        //         post.postMedia = SnapShareUtility.urlConverter(post.postMedia);
+            if (post?.postProfileImg)
+                post.postProfileImg = SnapShareUtility.urlConverter(post.postProfileImg);
 
-        //     if (post?.postProfileImg)
-        //         post.postProfileImg = SnapShareUtility.urlConverter(post.postProfileImg);
+            let comment = await this.commentService.getComments(post.postId, postCommentsLimit)
 
-        //     let comment = await this.commentService.getComments(post.postId, postCommentsLimit)
+            postContainer.push(post, comment)
+            feedContainer.push(postContainer);
+        }
 
-        //     postContainer.push(post, comment)
-        //     feedContainer.push(postContainer);
-        // }
-
-        // resp = { feedContainer };
+        resp = { feedContainer };
         return resp;
     }
 
 
 
-    async getDataToFeedAlgorith(timeDirection: boolean, postsByPage: number, offset = 0) {
+    async getPostsByPriority(postsByPage: number, offset: number = 0) {
 
         let resp: any;
         let standartLimit: number = 400;
-        let feedPostsQuery: string;
-        if (timeDirection) {
+        let cond: string = '';
+        let seenByUser: boolean = false;
+        let limit: number;
 
-            const lastSeenTimestamp = await this.entityManager
-                .createQueryBuilder(UserFeed, 'uf')
-                .select('uf.lastSeenTimestamp', 'lastSeenTimestamp')
-                .where('uf.userId = :userId', { userId: this.UserID })
-                .getRawOne();
+        let result: any[] = [];
 
+        const lastSeenTimestamp = await this.entityManager
+            .createQueryBuilder(UserFeed, 'uf')
+            .select('uf.lastSeenTimestamp', 'lastSeenTimestamp')
+            .where('uf.userId = :userId', { userId: this.UserID })
+            .getRawOne();
+
+        if (offset === 0) {
             if (lastSeenTimestamp) {
+                cond = `AND po."createdAt" >= '${lastSeenTimestamp}'`;
+                limit = standartLimit;
 
-                feedPostsQuery = `
-                WITH PostLikers AS (
-                SELECT
-                    pl."postId",
-                    l."username",
-                   ROW_NUMBER() OVER (PARTITION BY pl."postId" ORDER BY n."networkId" ASC) AS rn
-               FROM "postLike" pl
-               INNER JOIN "user" l ON l."userId" = pl."userId"
-               LEFT JOIN "network" n ON n."followeeId" = l."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-                WHERE pl.deleted = FALSE
-            )
-            SELECT 
-                po."postId" ,
-                po."postDescription",
-                po."commentsNr" AS "postCommentsNr",
-                po."likesNr" as "postLikesNr",
-                po.media as "postMedia",
-                po."userId" as "postUserId",
-                po."createdAt" as "postCreatedAt",
-                u."profileImg" as "postProfileImg",
-                u.username as "postLikersUsername",
-                CONCAT(u."firstName", ' ', u."lastName") AS "AccFullName",
-                CASE 
-                    WHEN pl."likeId" IS NOT NULL THEN 'true'
-                    ELSE 'false'
-                END AS "postLikedByUser",
-                STRING_AGG(pls."username", ', ') AS "postLikersUsernames",
-                'false' as "seenByUser"
-            FROM "post" po
-            INNER JOIN "user" u ON u."userId" = po."userId" AND u.archive = false
-            LEFT JOIN "network" n ON n."followeeId" = po."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-            LEFT JOIN "postLike" pl ON pl."userId" = n."followerId" AND pl."postId" = po."postId" AND pl.deleted = false
-            LEFT JOIN PostLikers pls ON pls."postId" = po."postId" AND pls.rn <= 3
-            WHERE po.archive = FALSE AND n."networkId" IS NOT NULL AND po."createdAt" >= '${lastSeenTimestamp}'
-            GROUP BY 
-                po."postId",
-                u."profileImg",
-                u.username,
-                u."firstName",
-                u."lastName",
-                pl."likeId"
-            ORDER BY po."createdAt" DESC
-               LIMIT ${standartLimit};`
+                result = await this.getDataToFeedAlgorithm(cond, seenByUser, limit);
+                resp = await this.feedAlgorithm(cond, postsByPage);
 
-                let queryResult = await this.entityManager.query(feedPostsQuery);
-                resp = await this.feedAlgorithm(queryResult, queryResult?.length)
+                if (resp?.length < postsByPage) {
 
-                if (queryResult?.length < postsByPage) {
+                    cond = ` AND po."createdAt" < '${lastSeenTimestamp}'`;
+                    seenByUser = true;
 
-                    let newLimit = postsByPage - queryResult?.length;
+                    result = await this.getDataToFeedAlgorithm(cond, seenByUser, limit);
 
-                    feedPostsQuery = `
-                    WITH PostLikers AS (
-                    SELECT
-                        pl."postId",
-                        l."username",
-                       ROW_NUMBER() OVER (PARTITION BY pl."postId" ORDER BY n."networkId" ASC) AS rn
-                   FROM "postLike" pl
-                   INNER JOIN "user" l ON l."userId" = pl."userId"
-                   LEFT JOIN "network" n ON n."followeeId" = l."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-                    WHERE pl.deleted = FALSE
-                )
-                SELECT 
-                    po."postId" ,
-                    po."postDescription",
-                    po."commentsNr" AS "postCommentsNr",
-                    po."likesNr" as "postLikesNr",
-                    po.media as "postMedia",
-                    po."userId" as "postUserId",
-                    po."createdAt" as "postCreatedAt",
-                    u."profileImg" as "postProfileImg",
-                    u.username as "postLikersUsername",
-                    CONCAT(u."firstName", ' ', u."lastName") AS "AccFullName",
-                    CASE 
-                        WHEN pl."likeId" IS NOT NULL THEN 'true'
-                        ELSE 'false'
-                    END AS "postLikedByUser",
-                    STRING_AGG(pls."username", ', ') AS "postLikersUsernames",
-                    'true' as "seenByUser"
-                FROM "post" po
-                INNER JOIN "user" u ON u."userId" = po."userId" AND u.archive = false
-                LEFT JOIN "network" n ON n."followeeId" = po."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-                LEFT JOIN "postLike" pl ON pl."userId" = n."followerId" AND pl."postId" = po."postId" AND pl.deleted = false
-                LEFT JOIN PostLikers pls ON pls."postId" = po."postId" AND pls.rn <= 3
-                WHERE po.archive = FALSE AND n."networkId" IS NOT NULL AND po."createdAt" < '${lastSeenTimestamp}'
-                GROUP BY 
-                    po."postId",
-                    u."profileImg",
-                    u.username,
-                    u."firstName",
-                    u."lastName",
-                    pl."likeId"
-                ORDER BY po."createdAt" DESC
-                   LIMIT ${standartLimit};`
-
-                    queryResult = await this.entityManager.query(feedPostsQuery);
-
-                    let oldPostsOrder = await this.feedAlgorithm(queryResult, postsByPage);
-                    resp.push(oldPostsOrder);
+                    limit = postsByPage - resp?.length;
+                    let topOldPosts = await this.feedAlgorithm(cond, limit);
+                    resp.push(topOldPosts);
 
                 }
             } else {
-                feedPostsQuery = `
-                WITH PostLikers AS (
-                SELECT
-                    pl."postId",
-                    l."username",
-                   ROW_NUMBER() OVER (PARTITION BY pl."postId" ORDER BY n."networkId" ASC) AS rn
-               FROM "postLike" pl
-               INNER JOIN "user" l ON l."userId" = pl."userId"
-               LEFT JOIN "network" n ON n."followeeId" = l."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-                WHERE pl.deleted = FALSE
-            )
-            SELECT 
-                po."postId" ,
-                po."postDescription",
-                po."commentsNr" AS "postCommentsNr",
-                po."likesNr" as "postLikesNr",
-                po.media as "postMedia",
-                po."userId" as "postUserId",
-                po."createdAt" as "postCreatedAt",
-                u."profileImg" as "postProfileImg",
-                u.username as "postLikersUsername",
-                CONCAT(u."firstName", ' ', u."lastName") AS "AccFullName",
-                CASE 
-                    WHEN pl."likeId" IS NOT NULL THEN 'true'
-                    ELSE 'false'
-                END AS "postLikedByUser",
-                STRING_AGG(pls."username", ', ') AS "postLikersUsernames",
-                'true' as "seenByUser"
-            FROM "post" po
-            INNER JOIN "user" u ON u."userId" = po."userId" AND u.archive = false
-            LEFT JOIN "network" n ON n."followeeId" = po."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-            LEFT JOIN "postLike" pl ON pl."userId" = n."followerId" AND pl."postId" = po."postId" AND pl.deleted = false
-            LEFT JOIN PostLikers pls ON pls."postId" = po."postId" AND pls.rn <= 3
-            WHERE po.archive = FALSE AND n."networkId" IS NOT NULL
-            GROUP BY 
-                po."postId",
-                u."profileImg",
-                u.username,
-                u."firstName",
-                u."lastName",
-                pl."likeId"
-            ORDER BY po."createdAt" DESC
-               LIMIT ${standartLimit};`
 
-                let queryResult = await this.entityManager.query(feedPostsQuery);
-                let oldPostsOrder = await this.feedAlgorithm(queryResult, postsByPage);
+
+                seenByUser = true;
+                limit = standartLimit;
+
+                result = await this.getDataToFeedAlgorithm(cond, seenByUser, limit);
+
+                let oldPostsOrder = await this.feedAlgorithm(result, postsByPage);
                 resp.push(oldPostsOrder);
             }
         } else {
+            seenByUser = true;
+            limit = standartLimit;
 
-
-            let feedPostsQuery = `
-            WITH PostLikers AS (
-            SELECT
-                pl."postId",
-                l."username",
-               ROW_NUMBER() OVER (PARTITION BY pl."postId" ORDER BY n."networkId" ASC) AS rn
-           FROM "postLike" pl
-           INNER JOIN "user" l ON l."userId" = pl."userId"
-           LEFT JOIN "network" n ON n."followeeId" = l."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-            WHERE pl.deleted = FALSE
-        )
-        SELECT 
-            po."postId" ,
-            po."postDescription",
-            po."commentsNr" AS "postCommentsNr",
-            po."likesNr" as "postLikesNr",
-            po.media as "postMedia",
-            po."userId" as "postUserId",
-            po."createdAt" as "postCreatedAt",
-            u."profileImg" as "postProfileImg",
-            u.username as "postLikersUsername",
-            CONCAT(u."firstName", ' ', u."lastName") AS "AccFullName",
-            CASE 
-                WHEN pl."likeId" IS NOT NULL THEN 'true'
-                ELSE 'false'
-            END AS "postLikedByUser",
-            STRING_AGG(pls."username", ', ') AS "postLikersUsernames",
-            'true' as "seenByUser"
-        FROM "post" po
-        INNER JOIN "user" u ON u."userId" = po."userId" AND u.archive = false
-        LEFT JOIN "network" n ON n."followeeId" = po."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
-        LEFT JOIN "postLike" pl ON pl."userId" = n."followerId" AND pl."postId" = po."postId" AND pl.deleted = false
-        LEFT JOIN PostLikers pls ON pls."postId" = po."postId" AND pls.rn <= 3
-        WHERE po.archive = FALSE AND n."networkId" IS NOT NULL
-        GROUP BY 
-            po."postId",
-            u."profileImg",
-            u.username,
-            u."firstName",
-            u."lastName",
-            pl."likeId"
-        ORDER BY po."createdAt" DESC
-           LIMIT ${standartLimit};`;
-
-            let queryResult = await this.entityManager.query(feedPostsQuery)
-
-            let oldPostsOrder = await this.feedAlgorithm(queryResult, postsByPage, offset) // limit, offset
+            result = await this.getDataToFeedAlgorithm(cond, seenByUser, limit);
+            // now here you also add the offset
+            let oldPostsOrder = await this.feedAlgorithm(result, postsByPage, offset);
             resp.push(oldPostsOrder);
         }
 
+
+        if (lastSeenTimestamp) {
+            await this.entityManager
+                .createQueryBuilder()
+                .update(UserFeed)
+                .set({
+                    lastSeenTimestamp: () => 'CURRENT_TIMESTAMP'
+                })
+                .where('userId = :userId', { userId: this.UserID })
+                .execute();
+        } else {
+            await this.entityManager
+                .createQueryBuilder()
+                .insert()
+                .into(UserFeed)
+                .values({
+                    userId: this.UserID,
+                    lastSeenTimestamp: () => 'CURRENT_TIMESTAMP'
+                })
+                .execute();
+        }
         return resp;
     }
+
+
+    async getDataToFeedAlgorithm(cond: string = '', seenByUser: boolean, limit: any, offset: any = 0) {
+
+        offset = offset ? ` OFFSET ${offset}` : '';
+        limit = limit ? ` LIMIT ${limit}` : '';
+
+        let feedPostsQuery = `
+        WITH PostLikers AS (
+        SELECT
+            pl."postId",
+            l."username",
+           ROW_NUMBER() OVER (PARTITION BY pl."postId" ORDER BY n."networkId" ASC) AS rn
+       FROM "postLike" pl
+       INNER JOIN "user" l ON l."userId" = pl."userId"
+       LEFT JOIN "network" n ON n."followeeId" = l."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
+        WHERE pl.deleted = FALSE
+    )
+    SELECT 
+        po."postId" ,
+        po."postDescription",
+        po."commentsNr" AS "postCommentsNr",
+        po."likesNr" as "postLikesNr",
+        po.media as "postMedia",
+        po."userId" as "postUserId",
+        po."createdAt" as "postCreatedAt",
+        u."profileImg" as "postProfileImg",
+        u.username as "postLikersUsername",
+        CONCAT(u."firstName", ' ', u."lastName") AS "AccFullName",
+        CASE 
+            WHEN pl."likeId" IS NOT NULL THEN 'true'
+            ELSE 'false'
+        END AS "postLikedByUser",
+        STRING_AGG(pls."username", ', ') AS "postLikersUsernames",
+        '${seenByUser}' as "seenByUser"
+    FROM "post" po
+    INNER JOIN "user" u ON u."userId" = po."userId" AND u.archive = false
+    LEFT JOIN "network" n ON n."followeeId" = po."userId" AND n.pending = FALSE AND n.deleted = FALSE AND n."followerId" = ${this.UserID}
+    LEFT JOIN "postLike" pl ON pl."userId" = n."followerId" AND pl."postId" = po."postId" AND pl.deleted = false
+    LEFT JOIN PostLikers pls ON pls."postId" = po."postId" AND pls.rn <= 3
+    WHERE po.archive = FALSE AND n."networkId" IS NOT NULL ${cond}
+    GROUP BY 
+        po."postId",
+        u."profileImg",
+        u.username,
+        u."firstName",
+        u."lastName",
+        pl."likeId"
+    ORDER BY po."createdAt" DESC
+       ${limit}
+       ${offset}`;
+
+        let resp = await this.entityManager.query(feedPostsQuery);
+        return resp;
+    }
+
 
 
 
     async feedAlgorithm(posts, postsByPage: number, offset: number = 0) {
-
-
     }
 
 
